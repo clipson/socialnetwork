5b03d2dd1fde9f6ada3ba71c1b338d527a4473b8
Christina Lipson <christinalipson29@gmail.com>
2015-03-06 02:09:43 -0500

    restructure

--- webapps/settings.py
+++ webapps/settings.py
@@ -64,7 +64,7 @@ ALLOWED_HOSTS = ['*']
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
 
-WSGI_APPLICATION = 'hw7.webapps.wsgi.application'
+WSGI_APPLICATION = 'webapps.wsgi.application'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
